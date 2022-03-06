86dcca6f5b5a73d59c85640054bb04481a6578a5
hugodg <hdg199@gmail.com>
2022-03-06 16:26:46 -0300

    changes before mpw5

--- netgen/ask-modulator_lvs.sh
+++
@@ -1,3 +0,0 @@
-netgen -batch lvs "/home/hugodg/projects_sky130/temp_sensor/ask_modulator/mag/ask-modulator.spice ask-modulator" "/home/hugodg/projects_sky130/temp_sensor/ask_modulator/xschem/simulation/ask-modulator.spice ask-modulator" /home/hugodg/sky130_workspace/skywater-pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl comp.out
-
-
